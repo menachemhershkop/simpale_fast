87e99e5b48618518bf6ab4f54ef9dfe4245441ff
mendy hershkop <hers.mm@gmail.com>
2025-11-12 17:22:38 +0200

    put method finally fix bugs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def load_data():
         return json.load(file)
 def save_data(data):
     with open("data.json", "w") as file:
-        json.dump(data,file)
+        json.dump(data,file, indent=4)
 @app.get("/")
 def main_page():
     return {"message":"hello"}
@@ -27,8 +27,8 @@ def update_data(item :Item):
     for i in items:
         if i['id'] == item.id:
             i['price']=item.price
-        save_data(items)
-        # return "Price update",items
+            save_data(items)
+            return "Price update",items
     raise HTTPException(status_code=404,detail="Not found")
 
 # if __name__=='__main__':
